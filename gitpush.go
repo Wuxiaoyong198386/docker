45b668a1525dee2dabafa36b0415a365771372c1
vip120@126.com <vip120@126.com>
2018-10-10 17:52:25 +0800

    测试

--- gitpush.go
+++ gitpush.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"os/exec"
@@ -67,25 +69,25 @@ func execCommand(commandName string, params []string) bool {
 	//显示运行的命令
 	fmt.Println(cmd.Args)
 
-	//stdout, err := cmd.StdoutPipe()
-	//
-	//if err != nil {
-	//	fmt.Println(err)
-	//	return false
-	//}
+	stdout, err := cmd.StdoutPipe()
+
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
 
 	cmd.Start()
 
-	//reader := bufio.NewReader(stdout)
+	reader := bufio.NewReader(stdout)
 
-	////实时循环读取输出流中的一行内容
-	//for {
-	//	_, err2 := reader.ReadString('\n')
-	//	if err2 != nil || io.EOF == err2 {
-	//		break
-	//	}
-	//	//fmt.Println(line)
-	//}
+	//实时循环读取输出流中的一行内容
+	for {
+		line, err2 := reader.ReadString('\n')
+		if err2 != nil || io.EOF == err2 {
+			break
+		}
+		fmt.Println(line)
+	}
 
 	cmd.Wait()
 	return true
